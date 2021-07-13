4bf6f6f14695fcff52c3bff890620210b5851a01
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2021-07-13 22:32:23 +0300

    Authtors

--- scenes/StartMenu.gd
+++ scenes/StartMenu.gd
@@ -7,3 +7,9 @@ func _on_Start_pressed():
 
 func _on_Settings_pressed():
 	$SettingsMenu.visible = true
+
+func _on_LookAuthors_pressed():
+	$Authors.visible = true
+
+func _on_Back_pressed():
+	$Authors.visible = false
